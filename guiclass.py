8f4830f09f52843b48f50b9b396ca84ada54867b
rad08d <rad08d@gmail.com>
2015-01-11 12:53:50 -0500

    Third commit
    This commit is aimed at using the pyGTK library. It's the first time
    I’ve ever played with it so it's not solid. Running the rssStart.py is
    the way to run the program from command line without any GUI. Run
    guyguiclass.py form any Debian installation or any computer that
    already has the pygtk and python installed to run the program with a
    GUI.

--- guiclass.py
+++ guiclass.py
@@ -4,6 +4,8 @@ import os
 import pygtk
 pygtk.require('2.0')
 import gtk
+from RssClass import Rss
+import sys
 
 class Base:
     def __init__(self):
@@ -11,21 +13,43 @@ class Base:
 	self.window.set_position(gtk.WIN_POS_CENTER)
 	self.window.set_title("Rss Reader")
         self.window.set_default_size(800,500)
-        dirName = os.path.dirname(__file__)
-        path = dirName + "/images/rssReader.jpg"
+        self.dirName = os.path.dirname(__file__)
+        path = self.dirName + "/images/rssReader.jpg"
+        self.tree = self.display_data()
 	self.window.set_icon_from_file(path)
-        self.startButton = gtk.Button("Start")
-        self.startButton.connect("clicked", self.startButtonCallBack)
-	self.box1 = gtk.VBox()
-        self.box1.pack_start(self.startButton)
-        self.window.add(self.box1)
+        self.window.add(self.tree)
 	self.window.show_all()
 	self.window.connect("destroy", self.destroy)
 
-    def startButtonCallBack(self, widget):
-        dirName = os.path.dirname(__file__)
-        script = dirName + "/rssStart.py"
-        os.system(script)
+    def display_data(self):
+        store = gtk.ListStore(str, str, str)
+        try:
+            path = self.dirName + "/rssWebSites.txt"
+            file = open(path)
+            sites = file.readlines()
+            siteStories = []
+            for site in sites:
+                rss = Rss(site)
+                rss.get_rss_into_articles()
+	        siteStories.append(rss)
+            try:
+                for collArticles in siteStories:
+                    for article in collArticles.articles:
+                        store.append([article.title, article.descr, article.pubDate]) 
+                tree = gtk.TreeView(store)
+                renderer = gtk.CellRendererText()
+                columnT = gtk.TreeViewColumn("Title", renderer, text=0)
+                tree.append_column(columnT)
+                columnDescr = gtk.TreeViewColumn("Description", renderer, text=4)
+                tree.append_column(columnDescr)
+                columnPubDate = gtk.TreeViewColumn("PubDate", renderer, text=2)
+                tree.append_column(columnPubDate)
+                return tree 
+            except:
+                print "display_data() error in guiclass.py. Error report: ", sys.exc_info()[1]
+        except IOError: 
+            print "Can't find website list file. Error: " , sys.exc_info()[1]
+
     def destroy(self, widget, data=None):
          gtk.main_quit()
     def main(self):
