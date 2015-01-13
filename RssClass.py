366f91c88005675cba389938b6a93c147bd675d6
rad08d <rad08d@gmail.com>
2015-01-12 19:21:53 -0500

    Fifth commit
    This commit adds an html parser to the RssClass module. This class
    allows the article class to download, parse, extract, and save the
    entire article to an article instance.

--- RssClass.py
+++ RssClass.py
@@ -1,5 +1,6 @@
 import sys
 import urllib2
+import HTMLParser
 import xml.etree.ElementTree as ET
 class Rss:
     """A class for handling RSS feeds"""
@@ -12,7 +13,7 @@ class Rss:
         self.articles = []
     def get_rss_into_articles(self):
 	response = urllib2.urlopen(self.url)
-	self.xml = response.read()
+	self.xml = response.read().decode('utf-8')
 	root = ET.fromstring(self.xml)
 
 	for item in root.findall(".//item"):
@@ -43,3 +44,34 @@ class Article:
         self.link = link
         self.descr = descr
         self.pubDate = pubDate
+        self.full_txt = ""
+
+    def get_full_txt(self):
+        response = urllib2.urlopen(self.link).read().decode('utf-8')
+        parser = RssHTMLParser()
+        parser.feed(response)
+        self.full_txt = parser.data
+
+class RssHTMLParser(HTMLParser.HTMLParser):
+    def __init__(self):
+        HTMLParser.HTMLParser.__init__(self)
+        self.is_start_p = False
+        self.is_end_p = False
+        self.data = []
+    def handle_starttag(self, tag, attrs):
+        if tag == 'p':
+            self.is_start_p = True
+        elif self.is_start_p and tag == 'a':
+            self.is_start_p = True
+        else:
+            self.is_start_p = False
+
+    def handle_endtag(self, tag):
+        if tag == 'p':
+            self.is_start_p = False
+        elif tag == 'a' and self.is_start_p:
+            self.is_start_p = True 
+
+    def handle_data(self, data):
+        if self.is_start_p:
+            self.data.append(data)
